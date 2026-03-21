5f5b8a239175e6332da8c505c4fcf97d3aefdc80
sunilreddy <sunilsmec@gmail.com>
2026-03-21 22:52:18 +0530

    change

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,7 @@ AMI_ID="ami-0220d79f3f480ecf5"
 
 for instance in $@
 do
-    instance$( aws ec2 run-instances \
+    INSTANCE_ID=$( aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type "t3.micro" \
     --security-group-ids $SG_ID \
