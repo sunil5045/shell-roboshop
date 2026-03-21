55775f0295c4ae312c75b05beccaa924a57542eb
sunilreddy <sunilsmec@gmail.com>
2026-03-21 22:53:52 +0530

    change

--- roboshop.sh
+++ roboshop.sh
@@ -13,7 +13,7 @@ do
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
     --query 'Instances[0].InstanceId' \
     --output text )
-    if [ $instance == "frontend"]; then
+    if [ $instance == "frontend" ]; then
         IP=$(
             aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
