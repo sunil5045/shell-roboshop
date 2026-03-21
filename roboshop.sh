6df973c7e6d3422da314187ea5c50b9addc15b24
sunilreddy <sunilsmec@gmail.com>
2026-03-21 22:48:00 +0530

    change

--- roboshop.sh
+++ roboshop.sh
@@ -6,11 +6,27 @@ AMI_ID="ami-0220d79f3f480ecf5"
 
 for instance in $@
 do
-    aws ec2 run-instances \
+    instance$( aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type "t3.micro" \
     --security-group-ids $SG_ID \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
     --query 'Instances[0].InstanceId' \
-    --output text
+    --output text )
+    if [ $instance == "frontend"]; then
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+        )
+    else
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )
+    fi
 done
+
