1bfc330bf9947ee92d67fca540d03ee3d27a1bd1
sunilreddy <sunilsmec@gmail.com>
2026-03-22 17:27:58 +0530

    change

--- catalogue.sh
+++ catalogue.sh
@@ -36,7 +36,7 @@ dnf install nodejs -y &>>$LOGS_FILE
 VALIDATE $? "Install NodeJS"
 
 id roboshop &>>$LOGS_FILE
-if[ $? -ne 0 ]; then
+if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
     VALIDATE $? "Creating system user"
 else
