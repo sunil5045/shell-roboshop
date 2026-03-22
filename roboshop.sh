c93881d76732d3b2b02c6d88f46fb8ff48afeb79
sunilreddy <sunilsmec@gmail.com>
2026-03-22 13:26:00 +0530

    change

--- roboshop.sh
+++ roboshop.sh
@@ -3,6 +3,8 @@
 #variables
 SG_ID="sg-028ddb0e8c0c3c494"
 AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z08342502MYJ3DHDYKBV1"
+DOMAIN_NAME="virtualmall.store"
 
 for instance in $@
 do
@@ -20,6 +22,7 @@ do
             --query 'Reservations[].Instances[].PublicIpAddress' \
             --output text
         )
+        RECORD_NAME="$DOMAIN_NAME" # virtualmall.store
     else
         IP=$(
             aws ec2 describe-instances \
@@ -27,8 +30,35 @@ do
             --query 'Reservations[].Instances[].PrivateIpAddress' \
             --output text
         )
+        RECORD_NAME="$INSTANCE.$DOMAIN_NAME" # mongodb.virtualmall.store
     fi
 
     echo "IP Adress: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Update A record",
+        "Changes": [
+            {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "'$RECORD_NAME'",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [
+                {
+                    "Value": "'$IP'"
+                }
+                ]
+            }
+            }
+        ]
+    }
+    '
+
+    echo "record update for $instance"
+    
 done
 
