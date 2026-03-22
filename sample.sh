4ef481ba0e47be93b299d6f2f2c2555f566457f4
sunilreddy <sunilsmec@gmail.com>
2026-03-22 07:48:40 +0530

    change

--- sample.sh
+++ sample.sh
@@ -5,6 +5,7 @@ AMI_ID="ami-0220d79f3f480ecf5"     # Replace with valid AMI ID
 INSTANCE_TYPE="t3.micro"
 KEY_NAME="sunildell"
 SECURITY_GROUP="sg-028ddb0e8c0c3c494" # Replace with valide Security group id
+SUBNET_ID="subnet-00cf3e53c22c09060"
 
 # Create EC2 instance
 aws ec2 run-instances \
