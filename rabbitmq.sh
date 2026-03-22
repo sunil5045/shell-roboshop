00af09d26ea69f7246d606464c864a5bc98224ec
sunilreddy <sunilsmec@gmail.com>
2026-03-22 20:41:09 +0530

    change

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,17 +1,18 @@
+
 #!/bin/bash
 
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-roboshop"
-LOGS_FILE="$LOGS_/$0.log"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 SCRIPT_DIR=$PWD
-MYSQL_HOST=mysql.virtualmall.store
+MYSQL_HOST=mysql.daws88s.online
 
 if [ $USERID -ne 0 ]; then
-    echo -e "$R Please run the script with root user access $N" | tee -a $LOGS_FILE
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
     exit 1
 fi
 
@@ -26,16 +27,16 @@ VALIDATE(){
     fi
 }
 
-cp $SCRIPT_DIR/rabbitmq.repo /etc/systemd/system/rabbitmq.repo
-VALIDATE $? "Added rabbitmq repo"
+cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+VALIDATE $? "Added RabbitMQ repo"
 
 dnf install rabbitmq-server -y &>>$LOGS_FILE
-VALIDATE $? "Installing rabbitMQ server"
+VALIDATE $? "Installing RabbitMQ server"
 
-systemctl enable rabbitmq-server 
+systemctl enable rabbitmq-server &>>$LOGS_FILE
 systemctl start rabbitmq-server
 VALIDATE $? "Enabled and started rabbitmq"
 
 rabbitmqctl add_user roboshop roboshop123 &>>$LOGS_FILE
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
-VALIDATE $? "Create a user and given permission"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOGS_FILE
+VALIDATE $? "created user and gien permissions"
