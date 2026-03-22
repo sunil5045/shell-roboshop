eb0c7ae270bbccbb4484792f0f538b315ed5cc3e
sunilreddy <sunilsmec@gmail.com>
2026-03-22 07:43:05 +0530

    change

---
+++ sample.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Variables
+AMI_ID="ami-0220d79f3f480ecf5"     # Replace with valid AMI ID
+INSTANCE_TYPE="t3.micro"
+KEY_NAME="sunildell"
+SECURITY_GROUP="sg-028ddb0e8c0c3c494" # Replace with valide Security group id
+
+# Create EC2 instance
+aws ec2 run-instances \
+  --image-id $AMI_ID \
+  --count 1 \
+  --instance-type $INSTANCE_TYPE \
+  --key-name $KEY_NAME \
+  --security-group-ids $SECURITY_GROUP \
+  --subnet-id $SUBNET_ID \
+  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=MyInstance}]'
+
+echo "EC2 instance creation triggered!"
+echo "Public IP   : $PUBLIC_IP"
+echo "Private IP  : $PRIVATE_IP"
